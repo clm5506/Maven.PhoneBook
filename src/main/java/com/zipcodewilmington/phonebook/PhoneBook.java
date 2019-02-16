e24b49db4a69c5c55688eb6eecf7142a73b3e754
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-16 15:46:03 -0500

    Phonebook lab is complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,91 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    SortedMap<String, ArrayList<String>> phoneBook = new TreeMap<String, ArrayList<String>>();
+
+   // SortedMap<String, String> phoneBook = new TreeMap<String, String>();
+
+    public void add(String name, String phoneNum) {
+
+       if(phoneBook.get(name) == null) {
+           ArrayList<String> phoneBookArrayList = new ArrayList<String>();
+           phoneBookArrayList.add(phoneNum);
+
+           phoneBook.put(name,phoneBookArrayList);
+
+       } else if(phoneBook.get(name) != null){
+          // for(Map.Entry<String, ArrayList<String>> entry : phoneBook.entrySet()){
+             //  if(entry.getKey().equals(name)) {
+               //    entry.getValue().add(phoneNum);
+           phoneBook.get(name).add(phoneNum);
+
+               }
+       }
+
+
+    public String lookUp(String name) {
+        if(phoneBook.get(name) != null ) {
+            ArrayList<String> phoneNumber = phoneBook.get(name);
+            String result = phoneNumber.toString().replace("[", "").replace("]", "");
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    public void removeRecord(String name) {
+
+        phoneBook.remove(name);
+
+    }
+
+    public void removeNumber(String phonNum) {
+
+        String name = reverseLookUp(phonNum);
+        ArrayList<String> phoneNumbers = phoneBook.get(name);
+
+       int index = phoneNumbers.indexOf(phonNum);
+
+       if(index != -1) {
+           phoneBook.get(name).remove(index);
+       }
+    }
+
+    public String reverseLookUp(String phoneNum) {
+        String name = "";
+        //ArrayList<String> newPhoneNum = new ArrayList<String>();
+        //newPhoneNum.add(phoneNum);
+        for(Map.Entry<String, ArrayList<String>> entry : phoneBook.entrySet()) {
+            for(int i = 0; i < entry.getValue().size(); i++) {
+
+                if (entry.getValue().indexOf(phoneNum) != -1) {
+                    name = entry.getKey();
+
+                }
+            }
+        }
+        return name;
+    }
+
+
+    public String display() {
+        String ans = "";
+
+        for(Map.Entry<String, ArrayList<String>> entry : phoneBook.entrySet()) {
+            ans = ans + entry.getKey() + " " + entry.getValue().toString()
+                    .replace("[","").replace("]","") + "\n";
+        }
+        System.out.print(ans);
+
+        return ans;
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,133 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String name = "Cristina McClintock";
+        String phoneNum = "8142791404";
+
+        //When
+        myPhoneBook.add(name,phoneNum);
+
+        //Then
+        Assert.assertEquals(phoneNum, myPhoneBook.lookUp(name));
+
+    }
+    @Test
+    public void testAddSecondPhoneNum() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String name = "Cristina Pierson";
+        String phoneNum = "999-333-1488";
+        String phoneNum2 = "767-444-3333";
+        String expected = "999-333-1488, 767-444-3333";
+
+        //When
+        myPhoneBook.add(name, phoneNum);
+        myPhoneBook.add(name, phoneNum2);
+
+        //Then
+        Assert.assertEquals(expected,myPhoneBook.lookUp(name));
+    }
+
+    @Test
+    public void testRemoveRecord() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String name = "Cristina Smith";
+        String phoneNum = "814-279-1405";
+
+        //When
+        myPhoneBook.add(name, phoneNum);
+        myPhoneBook.removeRecord(name);
+
+        //Then
+        Assert.assertEquals(null,myPhoneBook.lookUp(name));
+
+    }
+
+    @Test
+    public void testRemovePhoneNum() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String name = "Cristina Millz";
+        String phoneNum = "814-279-1488";
+        String phoneNum2 = "789-222-3333";
+
+        //When
+        myPhoneBook.add(name, phoneNum);
+        myPhoneBook.add(name, phoneNum2);
+        myPhoneBook.removeNumber(phoneNum2);
+
+
+        //Then
+        Assert.assertEquals(phoneNum,myPhoneBook.lookUp(name));
+    }
+
+    @Test
+    public void testLookUp() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String name = "Cristina MacIntosh";
+        String phoneNum = "814-279-1406";
+
+        //When
+        myPhoneBook.add(name, phoneNum);
+
+        //Then
+        Assert.assertEquals(phoneNum,myPhoneBook.lookUp(name));
+
+    }
+
+    @Test
+    public void testReverseLookUp() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String name = "Cristina Calvo";
+        String phoneNum = "814-279-1407";
+
+        //When
+        myPhoneBook.add(name, phoneNum);
+
+        //Then
+        Assert.assertEquals(name, myPhoneBook.reverseLookUp(phoneNum));
+
+    }
+
+    @Test
+    public void testDisplay() {
+        //Given
+        PhoneBook myPhoneBook = new PhoneBook();
+        String nameA = "Ann Anderson";
+        String nameB = "Bill Billson";
+        String nameC = "Cristina Cruz";
+        String aPhoneNum = "902-122-3232";
+        String bPhoneNum = "222-343-1212";
+        String cPhoneNum = "814-279-1408";
+        String expectedDisplayResult = "Ann Anderson 902-122-3232\n" +
+                                       "Bill Billson 222-343-1212\n" +
+                                       "Cristina Cruz 814-279-1408\n";
+
+        //When
+        myPhoneBook.add(nameA, aPhoneNum);;
+        myPhoneBook.add(nameB, bPhoneNum);
+        myPhoneBook.add(nameC, cPhoneNum);
+
+        //Then
+        Assert.assertEquals(expectedDisplayResult, myPhoneBook.display());
+
+    }
+
+
+
 }
